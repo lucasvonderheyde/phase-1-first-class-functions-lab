38e988aca977cef068fe8b780b1554cddcdb229c
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-08-22 11:41:34 -0400

    finish lab' git push

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(scuberDrivers) {
+    return scuberDrivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(scuberDrivers) {
+    return scuberDrivers.slice(-2, scuberDrivers.length + 1)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultipler) {
+    return function (fare) {
+        return fare * fareMultipler;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, selectDriversFunction) {
+    return selectDriversFunction(drivers)
+}
+
